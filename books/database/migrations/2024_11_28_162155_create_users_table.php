07c2f2ef9406b12e8160d2c74eed2585863efb0d
StupachenkoArtem <stupachenko-03@mail.ru>
2024-11-28 20:36:44 +0300

    Добавление миграций: 1. Книги 2. Пользователи 3. Бронирования

--- books/database/migrations/2024_11_28_162155_create_users_table.php
+++ books/database/migrations/2024_11_28_162155_create_users_table.php
@@ -10,11 +10,11 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id(); // Создает поле id (INT, PRIMARY KEY, AUTO_INCREMENT)
-            $table->string('name'); // Создает поле name (VARCHAR(255))
-            $table->string('email')->unique(); // Создает поле email (VARCHAR(255), UNIQUE)
-            $table->enum('role', ['admin', 'user']); // Создает поле role (ENUM)
-            $table->timestamps(); // Создает поля created_at и updated_at
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->enum('role', ['admin', 'user']);
+            $table->timestamps();
         });
     }
 
---
+++ books/database/migrations/2024_11_28_171325_create_books_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('book', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('author');
+            $table->year('publication_year');
+            $table->boolean('is_available')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('book');
+    }
+}
